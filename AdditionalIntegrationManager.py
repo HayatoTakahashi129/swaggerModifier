baa6e7faf20c735d84ab3114c298840ebac19c27
HayatoTakahashi <hayato.012995@gmail.com>
2022-08-15 07:50:40 +0900

    add integrations

---
+++ AdditionalIntegrationManager.py
@@ -0,0 +1,101 @@
+from typing import List
+
+import config
+from SwaggerManager import SwaggerManager
+
+security_schema_name = 'ID-Token'
+security_integration = {
+    'x-amazon-apigateway-authtype': 'cognito_user_pools',
+    'x-amazon-apigateway-authorizer': {
+        'type': 'cognito_user_pools',
+        'providerARNs': [
+            # fixme: change cognito id to variable
+            'arn:aws:cognito-idp:${AWS::Region}:${AWS::AccountId}:userpool/${AWS::Region}_EICVnWjIy'
+        ],
+        'identityValidationExpression': '.*'
+    }
+}
+default_headers = ['Content-Type', 'X-Amz-Date', 'X-Api-Key', 'X-Amz-Security-Token']
+
+
+class AdditionalIntegrationManager(SwaggerManager):
+    def __init__(self, swagger: dict):
+        super().__init__(swagger)
+        self.swagger: dict = swagger
+
+    def __get_all_methods(self, path: str):
+        methods: List[str] = self.get_all_contained_service_method(path)
+        methods.append('options')
+        methods = [method.upper() for method in methods]
+        return methods
+
+    def __get_option_integration(self, path: str) -> dict:
+        methods: List[str] = self.__get_all_methods(path)
+        headers = default_headers
+        if self.has_security(path, self.get_all_contained_service_method(path)[0]):
+            headers.append('Authorization')
+        return {
+            'x-amazon-apigateway-integration': {
+                'responses': {
+                    'default': {
+                        'statusCode': '200',
+                        'responseParameters': {
+                            'method.response.header.Access-Control-Allow-Headers': "'{headers}'".format(
+                                headers=','.join(headers)),
+                            'method.response.header.Access-Control-Allow-Methods': "'{methods}'".format(
+                                methods=','.join(methods)),
+                            # fixme: set origin fqdn
+                            'method.response.header.Access-Control-Allow-Origin': "'*'"
+                        },
+                        'responseTemplates': {
+                            'application/json': '{}'
+                        }
+                    }
+                },
+                'requestTemplates': {
+                    'application/json': '{"statusCode": 200}'
+                },
+                'passthroughBehavior': 'when_no_match',
+                'type': 'mock'
+            }
+        }
+
+    def __add_integration_to_option_method(self, path: str):
+        option_integration: dict = self.__get_option_integration(path)
+        self.swagger['paths'][path]['options'].update(option_integration)
+
+    def __get_lambda_name(self, path: str, method: str) -> str:
+        tags: List[str] = self.get_tags(path, method)
+        # use first set tags. fixme: change to use tag which is define as service tag.
+        service_tag: str = tags[0]
+        service_name: str = self.get_service_name()
+        return service_name + '/' + service_tag + '-' + config.ENV
+
+    def __add_integration_to_service_method(self, path: str):
+        methods: List[str] = self.get_all_contained_service_method(path)
+        for method in methods:
+            lambda_name: str = self.__get_lambda_name(path, method)
+            self.swagger['paths'][path][method].update({
+                'x-amazon-apigateway-integration': {
+                    'uri': 'arn:${AWS::Partition}:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/arn:${AWS::Partition}:lambda:${AWS::Region}:${AWS::AccountId}:function:{lambdaName}/invocations'.replace(
+                        '{lambdaName}', lambda_name),
+                    'passthroughBehavior': 'when_no_templates',
+                    'httpMethod': 'POST',
+                    'type': 'aws_proxy'
+                }
+            })
+
+    # add integration for lambda
+
+    def __add_security_integration(self):
+        self.swagger['components']['securitySchemes'][security_schema_name].update(security_integration)
+
+    def add_amazon_apigateway_integration(self) -> dict:
+        self.__add_security_integration()
+
+        path_list: List[str] = self.get_all_paths()
+        for path in path_list:
+            self.__add_integration_to_option_method(path)
+            self.__add_integration_to_service_method(path)
+
+        return self.swagger
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from argparse import ArgumentParser
 import yaml
 
 import config
+from AdditionalIntegrationManager import AdditionalIntegrationManager
+from OptionMethodManager import OptionMethodManager
 
 
 def get_input_yaml(yaml_path: str) -> dict:
@@ -15,11 +17,16 @@ def get_input_yaml(yaml_path: str) -> dict:
     return swagger_dict
 
 
+def write_swagger(swagger_dict: dict, yaml_path: str):
+    with open(yaml_path, 'w') as file:
+        yaml.dump(swagger_dict, file)
+
+
 def get_options():
     argparser = ArgumentParser()
-    argparser.add_argument('-i', '--input', type=str, help='Set input swagger file path to create.')
-    argparser.add_argument('-o', '--output', type=str, help='Set output file path.')
-    argparser.add_argument('-e''--env', type=str, default='dev', help='Set environment to create swagger file')
+    argparser.add_argument('-i', '--input', type=str, required=True, help='Set input swagger file path to create.')
+    argparser.add_argument('-o', '--output', type=str, required=True, help='Set output file path.')
+    argparser.add_argument('-e', '--env', type=str, default='dev', help='Set environment to create swagger file')
     return argparser.parse_args()
 
 
@@ -27,3 +34,8 @@ if __name__ == '__main__':
     args = get_options()
     config.ENV = args.env
     swagger = get_input_yaml(args.input)
+    optionManager = OptionMethodManager(swagger)
+    swagger = optionManager.add_option_methods()
+    additionalIntegrationManager = AdditionalIntegrationManager(swagger)
+    swagger = additionalIntegrationManager.add_amazon_apigateway_integration()
+    write_swagger(swagger, args.output)
